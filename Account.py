a06c5cea9d46e751ddfceb5126c8b07ba158e2f7
pigman <asddfth@naver.com>
2021-02-11 12:57:11 +0900

    초기커밋

---
+++ Account.py
@@ -0,0 +1,7 @@
+class Account:
+    num_accounts = 0
+    def __init__(self, name):
+        self.name = name
+        Account.num_accounts += 1
+    def __del__(self):
+        Account.num_accounts -= 1
