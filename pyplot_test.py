136ccf4ae878c628e95274f38e7fb0ca4d2e0377
asddfth <34783180+asddfth@users.noreply.github.com>
2021-02-16 12:19:20 +0900

    수정

---
+++ pyplot_test.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+'''
+x = range(0, 100)
+y = [v*v for v in x]
+plt.plot(x, y)
+plt.show()
+'''
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 1, 1)
+ax2 = fig.add_subplot(2, 1, 2)
+
+x = range(0, 100)
+y = [v*v for v in x]
+
+ax1.plot(x, y)
+ax2.bar(x, y)
+
+plt.show()
